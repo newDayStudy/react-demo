7e5f976647823f85b09ffe98ad2f104571b24c9d
gu_shiyuan <429382356@qq.com>
2022-03-07 11:28:42 +0800

    文件x修改测试

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -1,39 +1,43 @@
 import React from 'react'
 import Login from '@/views/login'
 import Home from '@/views/home'
-// import Permission from '@/views/permission'
+import Permission from '@/views/permission'
 import UserManager from '@/views/home/module/UserManager'
 import MenuManager from '@/views/home/module/MenuManager'
 import ArticleManager from '@/views/home/module/ArticleManager'
 const routes = [
   {
+    path: '',
+    component: <Permission />
+  },
+  {
     path: '/login',
     name: 'login',
-    component: <Login/>,
+    component: <Login />,
     auth: false
   },
   {
     path: '/home',
     name: 'home',
-    component: <Home/>,
+    component: <Home />,
     auth: true,
     children: [
       {
         path: 'userManager',
         name: 'userManager',
-        component: <UserManager/>,
+        component: <UserManager />,
         auth: true
       },
       {
         path: 'menuManager',
         name: 'menuManager',
-        component: <MenuManager/>,
+        component: <MenuManager />,
         auth: true
       },
       {
         path: 'articleManager',
         name: 'articleManager',
-        component: <ArticleManager/>,
+        component: <ArticleManager />,
         auth: true
       }
     ]
