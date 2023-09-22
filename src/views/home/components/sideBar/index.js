aabf5e3c42742d3d43d4ef1fb3f22d47b1f95e64
gu_shiyuan <you@example.com>
2023-09-22 18:21:27 +0800

    fix: 组件

--- src/views/home/components/sideBar/index.js
+++ src/views/home/components/sideBar/index.js
@@ -7,15 +7,21 @@ import '@/assets/sideBar.less'
 import {useDispatch} from "react-redux";
 import {add} from "@/redux/modules/tabs-store";
 const { Sider } = Layout;
+const flat = (data) => {
+    return data.reduce((prev, cur) => {
+        return prev.concat(cur.children ? flat(cur.children) : cur)
+    }, [])
+}
 function SideBar(props) {
-  const [defaultSelectedKeys, setDefaultSelectedKeys] = useState([])
+  const [defaultSelectedKeys, setDefaultSelectedKeys] = useState([]);
+  const [openKeys, setOpenKeys] = useState(['subMenu'])
   const Location = useLocation();
   const router= useNavigate();
   const dispatch = useDispatch()
   useEffect(() => {
     setDefaultSelectedKeys([Location.pathname]);
     if (Location.pathname) {
-        const tabData = items.find(item => item.key == Location.pathname)
+        const tabData = flat(items).find(item => item.key == Location.pathname)
         if (tabData) {
             dispatch(add(tabData))
         }
@@ -37,7 +43,7 @@ function SideBar(props) {
               }
           </div>
           <Sider theme="dark" trigger={null} collapsible collapsed={props.collapsed}>
-              <Menu theme="dark" mode="inline" selectedKeys={defaultSelectedKeys} items={items} onClick={(e) => onClick(e)}></Menu>
+              <Menu theme="dark" mode="inline" openKeys={openKeys} selectedKeys={defaultSelectedKeys} items={items} onClick={(e) => onClick(e)}></Menu>
           </Sider>
       </div>
   )
